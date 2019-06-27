0ab0e6890606c5e841f2e3ed74d2c03d4d8545f5
Rodrigo Souza <rodrigocarlos1883@gmail.com>
2019-06-27 18:00:38 -0300

    Programa simples, Média de Aluno

---
+++ fundamentos/mediaAluno.js
@@ -0,0 +1,13 @@
+let a = 9.0
+let b = 9.5
+let c = 7.5
+
+const media = (a + b + c)/3
+
+if (media >= 7) 
+
+console.log(media.toFixed(2) +' Aluno aprovado')// toFixed(2) delimita em 2 números após casa decimal
+
+else
+
+ console.log(media.toFixed(2) +' Aluno reprovado')
