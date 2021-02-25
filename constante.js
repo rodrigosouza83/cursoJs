8bd20241aee1a08d252d401ceece556d6ae3253f
rodrigosouza83 <rodrigocarlos1883@gmail.com>
2021-02-25 01:36:16 -0300

    Básico jS

---
+++ constante.js
@@ -0,0 +1,14 @@
+//Constantes não podem ser alteradas ao longo do código.
+
+const valor1 = 80;
+const valor2 = 42;
+
+const calculoValores = valor1 * valor2;
+const calculoValores2 = valor1 + valor2;
+const calculoValores3 = valor1 - valor2;
+const calculoValores4 = valor1 / valor2;
+
+console.log(calculoValores);
+console.log(calculoValores2);
+console.log(calculoValores3);
+console.log(typeof (calculoValores4));
