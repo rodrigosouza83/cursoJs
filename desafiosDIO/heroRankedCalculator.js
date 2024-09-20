4a4a5429b1f344191fba1bc74750a6343212dd1a
Rodrigo Souza <rodrigocarlos1883@gmail.com>
2024-09-20 09:01:16 -0300

    Desafio ranking hero DIO

---
+++ desafiosDIO/heroRankedCalculator.js
@@ -0,0 +1,30 @@
+function calcularRanking(vitorias, derrotas) {
+
+    let saldoVitorias = vitorias - derrotas;
+
+    let nivel = '';
+
+    if (saldoVitorias < 10) {
+        nivel = 'Ferro';
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = 'Prata';
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = 'Lendário';
+    } else if (saldoVitorias >= 101) {
+        nivel = 'Imortal';
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso
+let vitorias = 100;
+let derrotas = 10;
+let resultado = calcularRanking(vitorias, derrotas);
+console.log(resultado);
